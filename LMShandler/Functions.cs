c4f04c31645d6744f91959a0af58d491ad37f7da
aotuo <aotuo.zhang@renweb.com>
2015-09-22 18:04:58 -0500

    update 2

---
+++ LMShandler/Functions.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.IO;
+using firebase1.DocumentDB;
+using Microsoft.Azure.WebJobs;
+
+namespace LMShandler
+{
+    public class Functions
+    {
+        private static IDocumentDB _IDocumentDb;
+
+        static Functions()
+        {
+            _IDocumentDb = new firebase1.DocumentDB.DocumentDB();
+        }
+
+        [NoAutomaticTrigger]
+        public static void ManualTrigger(TextWriter log)
+        {
+            log.WriteLine("Function is invoked at {0}", DateTime.Now);
+            _IDocumentDb.UpdateDocument().Wait();
+            log.WriteLine("Function is end at {0}", DateTime.Now);
+        }
+    }
+}
---
+++ LMShandler/Program.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Azure.WebJobs;
+
+namespace LMShandler
+{
+    // To learn more about Microsoft Azure WebJobs SDK, please see http://go.microsoft.com/fwlink/?LinkID=320976
+    class Program
+    {
+        // Please set the following connection strings in app.config for this WebJob to run:
+        // AzureWebJobsDashboard and AzureWebJobsStorage
+        static void Main()
+        {
+            var host = new JobHost();
+            // The following code will invoke a function called ManualTrigger and 
+            // pass in data (value in this case) to the function
+            host.Call(typeof(Functions).GetMethod("ManualTrigger"));
+        }
+    }
+}
+
