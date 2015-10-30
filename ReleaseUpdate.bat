23c4920adc8289cee3bb2c1598ec47e10470a222
aotuo <aotuo.zhang@renweb.com>
2015-10-30 12:43:32 -0500

    8

--- ReleaseUpdate.bat
+++ ReleaseUpdate.bat
@@ -1,9 +1,5 @@
 @echo off
 SETLOCAL
-Set  n=1
-echo %n%
-set /a n=n+2
-echo %n%
 echo %~1
 Set /a z=%1+1
 curl --verbose --request PUT --data %z% --Header "Content-Type: text/plain" http://aotuo:1q2w3e4r@localhost:8080/httpAuth/app/rest/projects/lms/parameters/az
