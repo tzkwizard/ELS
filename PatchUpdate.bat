0e58a2e8e332f8166aab7a3f954a4eb8053b814e
aotuo <aotuo.zhang@renweb.com>
2015-10-30 13:03:06 -0500

    11

--- PatchUpdate.bat
+++ PatchUpdate.bat
@@ -2,5 +2,6 @@
 SETLOCAL
 echo %~1
 Set /a z=%1+1
+if %~2==1 curl --verbose --request PUT --data %~1 --Header "Content-Type: text/plain" http://aotuo:1q2w3e4r@localhost:8080/httpAuth/app/rest/projects/lms/parameters/cloud.patch
 if %~2==1 curl --verbose --request PUT --data %z% --Header "Content-Type: text/plain" http://aotuo:1q2w3e4r@localhost:8080/httpAuth/app/rest/projects/lms/parameters/cloud.pre.patch
 ENDLOCAL
