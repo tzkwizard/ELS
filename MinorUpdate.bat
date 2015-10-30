8063af54aac09977b4a086d0dd9095ff370ec70a
aotuo <aotuo.zhang@renweb.com>
2015-10-30 12:54:03 -0500

    10

--- MinorUpdate.bat
+++ MinorUpdate.bat
@@ -2,5 +2,5 @@
 SETLOCAL
 echo %~1
 Set /a z=%1+1
-curl --verbose --request PUT --data %z% --Header "Content-Type: text/plain" http://aotuo:1q2w3e4r@localhost:8080/httpAuth/app/rest/projects/lms/parameters/cloud.minor
+if %~2==1 curl --verbose --request PUT --data %z% --Header "Content-Type: text/plain" http://aotuo:1q2w3e4r@localhost:8080/httpAuth/app/rest/projects/lms/parameters/cloud.pre.minor
 ENDLOCAL
