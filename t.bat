13da718fae11f55023aa3899cb64a336db7eddfd
aotuo <aotuo.zhang@renweb.com>
2015-10-30 12:40:48 -0500

    6

--- t.bat
+++ t.bat
@@ -5,6 +5,6 @@ echo %n%
 set /a n=n+2
 echo %n%
 echo %~1
-set az=5
-echo %az%
+Set /a z=%1+1
+curl --verbose --request PUT --data %z% --Header "Content-Type: text/plain" http://aotuo:1q2w3e4r@localhost:8080/httpAuth/app/rest/projects/lms/parameters/az
 ENDLOCAL
