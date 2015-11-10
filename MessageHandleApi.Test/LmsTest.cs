bf823b0e2f55c13625d76de723985412a971f603
aotuo <aotuo.zhang@renweb.com>
2015-11-10 14:29:16 -0600

    nunit 1

---
+++ MessageHandleApi.Test/LmsTest.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Web.Http;
+using System.Web.Http.Results;
+using LMS.Common.Models;
+using LMS.Common.Models.Api;
+using LMS.Common.Service;
+using MessageHandleApi.Controllers;
+using NSubstitute;
+using NUnit.Framework;
+
+namespace MessageHandleApi.Test
+{
+    [TestFixture]
+    public class LmsTest
+    {
+        private static LMSController _controller;
+        private static IDbService _iDbService;
+        public LmsTest()
+        {
+            IQueueService iQueueService = new QueueService();
+            //IDbService iDbService = new DbService();
+            _iDbService = Substitute.For<IDbService>();
+            IAzureStorageService iAzureStorageService = new AzureStorageService();
+            _controller = new LMSController(iQueueService, _iDbService, iAzureStorageService);
+        }
+
+        [Test]
+        public void Get_Test()
+        {
+            var time = (long)(DateTime.UtcNow.Subtract(new DateTime(1970, 1, 1))).TotalMilliseconds;
+
+            _iDbService.GetMoreList("LMS/tst-azhang1/HS/Java/-K2hf2hPR4FZNk6ipEyn", time).Returns(new LMSresult
+            {
+                time = 12,
+                list = new List<PostMessage>(),
+                moreData = true
+            });
+
+
+            IHttpActionResult result = _controller.Get("LMS/tst-azhang1/HS/Java/-K2hf2hPR4FZNk6ipEyn", time);
+            Assert.IsInstanceOf<OkNegotiatedContentResult<LMSresult>>(result);
+            IHttpActionResult result2 = _controller.Get("LMS/tst-azhang1/HS/Java/-K2hf2hPR4FZNk6ipEyn", 0);
+            Assert.IsInstanceOf<OkNegotiatedContentResult<LMSresult>>(result2);
+
+            IHttpActionResult result3 = _controller.Get("LM", 0);
+            Assert.IsInstanceOf<OkNegotiatedContentResult<LMSresult>>(result3);
+        }
+
+        [Test]
+        public void GetChat_Test()
+        {
+              var time = (long)(DateTime.UtcNow.Subtract(new DateTime(1970, 1, 1))).TotalMilliseconds;
+
+              IHttpActionResult result = _controller.GetChat("-K0D7IEKYJeQpB01dMgF", time);
+              Assert.IsInstanceOf<OkNegotiatedContentResult<LMSChatresult>>(result);
+
+              IHttpActionResult result2 = _controller.GetChat("-K0D7IEKYJeQpB01dMgF", 0);
+              Assert.IsInstanceOf<OkNegotiatedContentResult<LMSChatresult>>(result2);
+
+              IHttpActionResult result3 = _controller.GetChat("", time);
+              Assert.IsInstanceOf<OkNegotiatedContentResult<LMSChatresult>>(result3);
+        }
+    }
+}
